00a8bf48f4780bf4440d48ecdedb66e29a051749
William Fergus <Will.Fergus@iam.local>
2018-10-22 17:48:29 -0500

    adds broken movement script

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    private bool facingRight = true;
+    public Rigidbody2D rb2d;
+	void Start () {
+		
+	}
+	
+	
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+	}
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
