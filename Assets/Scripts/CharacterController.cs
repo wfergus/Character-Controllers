1c5a6c98dc4a73bfefd136c7098bf1e0a67e9e08
William Fergus <Will.Fergus@iam.local>
2018-10-24 14:18:45 -0500

    adds finished jump/fall animations

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,19 +8,42 @@ public class CharacterController : MonoBehaviour {
     public float maxSpeed = 10f;
     private bool facingRight = true;
     public Rigidbody2D rb2d;
+    Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 350f;
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", Mathf.Abs(move));
+
         rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
         if (move > 0 && !facingRight)
             Flip();
         else if (move < 0 && facingRight)
             Flip();
 	}
+    void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+        }
+    }
     void Flip()
     {
         facingRight = !facingRight;
